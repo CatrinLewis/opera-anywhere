bb738a71b8a7bf65c6b738853b8d512c8a306635
CatrinLewis <catrin_elenid@hotmail.com>
2021-05-26 13:06:06 +0100

    model amendment

--- events/models.py
+++ events/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     production = models.CharField(max_length=100)
-    slug = models.SlugField()
+    performance_date = models.TextField(blank=True)
+    location = models.TextField(blank=True)
     body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    posting_date = models.DateTimeField(auto_now_add=True)
     #complete this database later
     #production to be a dropdown menu
     #slug based on production name won't work
