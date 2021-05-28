5f2d610be5c6f7ea3b71b82362330ff1b22afe76
CatrinLewis <catrin_elenid@hotmail.com>
2021-05-28 19:43:45 +0100

    home page with css

--- events/models.py
+++ events/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     production = models.CharField(max_length=100)
+    thumb = models.ImageField(default='default.png', blank=True)
     performance_date = models.TextField(blank=True)
     location = models.TextField(blank=True)
     body = models.TextField()
