529b594614c3857bbb11613307f4a161cea4416f
CatrinLewis <catrin_elenid@hotmail.com>
2021-06-19 09:54:38 +0100

    working summernote

--- oaevents/urls.py
+++ oaevents/urls.py
@@ -21,9 +21,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 from events import views as event_views
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
